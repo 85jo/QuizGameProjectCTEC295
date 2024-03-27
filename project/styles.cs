e9c52f36c8de010490603f54771ea3c1da472958
85jo <97642836+85jo@users.noreply.github.com>
2024-03-26 20:47:15 -0500

    upload project
    uploaded project

---
+++ project/styles.cs
@@ -0,0 +1,69 @@
+body {
+    background-color: #475b76;
+}
+.quiz-card {
+    min-width: 500px;
+    max-width: 80%;
+    min-height: 400px;
+    background-color: #243854;
+    color: #ffffff;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0,0,0,0.1);
+}
+.answer-btn, #submitBtn {
+    font-weight: bold;
+    margin: 5px;
+    box-shadow: 2px 2px 4px rgba(0,0,0,0.2);
+    background-color: #2e5374;
+    color: #dcd8f1;
+    transition: transform 0.1s ease;
+}
+.answer-btn:hover, #submitBtn:hover {
+    transform: translateY(-2px);
+    background-color: #a8cbff;
+    color: #000001;
+}
+.selected {
+    background-color: #6aa0b7;
+}
+#submitBtn {
+    background-color: #4a627b;
+    color: white;
+    margin-top: 10px;
+}
+#scoreBox {
+    padding: 10px;
+    background-color: #ffffff;
+    color: #05505c;
+    border-radius: 5px;
+    box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+    text-align: center;
+}
+@media (max-width: 576px) {
+    .quiz-card {
+        min-width: 90%;
+        min-height: 300px;
+    }
+    #scoreBox {
+        margin-bottom: 15px;
+    }
+}
+.answers-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 10px;
+}
+.answer-btn {
+    flex-grow: 1;
+    min-width: calc(100% - 20px);
+    text-align: center;
+}
+@media (max-width: 690px) {
+    .answers-container {
+        flex-direction: column;
+    }
+    .answer-btn {
+        max-width: 100%;
+    }
+}
